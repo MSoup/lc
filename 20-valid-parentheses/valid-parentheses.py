f64e2a920002189d39dd555697624aa4fea7cd11
Daven <19704779+MSoup@users.noreply.github.com>
2024-04-02 23:55:15 +0900

    Time: 34 ms (67.61%) | Memory: 16.6 MB (23.26%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        opening_pairs = {"{": "}", "[": "]", "(":")" }
+        stack = []
+        for bracket in s:
+            if bracket in opening_pairs:
+                stack.append(bracket) 
+            elif not stack:
+                    return False
+            elif opening_pairs[stack.pop()] != bracket:
+                return False
+        return stack == [] 
