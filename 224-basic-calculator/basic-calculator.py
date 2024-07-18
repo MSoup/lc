4a46df2376413357a6471535a771e03fa04e0576
Daven <19704779+MSoup@users.noreply.github.com>
2024-07-18 14:18:28 +0900

    Time: 56 ms (85.91%) | Memory: 17.9 MB (89.34%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,41 +1,35 @@
-
-class CallStack:
-    def __init__(self, value, sign):
-        self.value = value
-        self.sign = sign
-
 class Solution:
     def calculate(self, s: str) -> int:
-        current = result = 0
+        total = current_num = 0
         sign = 1
         stack = []
 
-        i = 0
-        while i < len(s):
-            current_char = s[i]
-            # Accumulate number
-            if s[i].isdigit():
-                current = current * 10 + int(s[i])
-            elif s[i] in "+-":
-                # apply computation of prev
-                result += sign * current
-                sign = -1 if s[i] == "-" else 1
-                current = 0
-            elif s[i] == "(":
-                # append current result then start again
-                stack.append(CallStack(result, sign))
-                
-                # reset sign and result
+        for char in s:
+            if char.isdigit():
+                # need to start building the number
+                current_num = current_num * 10 + int(char)
+            elif char == "+":
+                # current_num won't grow anymore
+                total += sign * current_num
+                current_num = 0
                 sign = 1
-                result = 0
-            elif s[i] == ")":
-                # apply cur val to prev
-                result += sign * current
-                stackTop = stack.pop()
-                result *= stackTop.sign
-                result += stackTop.value
+            elif char == "-":
+                total += sign * current_num
+                current_num = 0
+                sign = -1
+            elif char == "(":
+                # save current progress
+                stack.append(total)
+                stack.append(sign)
 
-                current = 0
-            i += 1
+                sign = 1
+                total = 0
 
-        return result + (sign * current)
+            elif char == ")":
+                total += sign * current_num
+                total *= stack.pop()
+                total += stack.pop()
+
+
+                current_num = 0
+        return total + current_num * sign
