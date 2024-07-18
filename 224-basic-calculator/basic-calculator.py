ef1149cde1be35b5aff5b77842ec89799f0bd84e
Daven <19704779+MSoup@users.noreply.github.com>
2024-07-18 13:23:57 +0900

    Time: 74 ms (46.73%) | Memory: 18 MB (49.52%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.py
@@ -0,0 +1,45 @@
+
+class CallStack:
+    def __init__(self, value, sign):
+        self.value = value
+        self.sign = sign
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        current = result = 0
+        sign = 1
+        stack = []
+
+        i = 0
+        while i < len(s):
+            current_char = s[i]
+            # Accumulate number
+            if s[i].isdigit():
+                current = current * 10 + int(s[i])
+            elif s[i] == "+":
+                # apply computation of prev
+                result += sign * current
+                sign = 1
+                current = 0
+            elif s[i] == "-":
+                result += sign * current
+                sign = -1
+                current = 0
+
+            elif s[i] == "(":
+                # append current result then start again
+                stack.append(CallStack(result, sign))
+                
+                sign = 1
+                result = 0
+            elif s[i] == ")":
+                # apply cur val to prev
+                result += sign * current
+                stackTop = stack.pop()
+                result *= stackTop.sign
+                result += stackTop.value
+
+                current = 0
+            i += 1
+
+        return result + (sign * current)
