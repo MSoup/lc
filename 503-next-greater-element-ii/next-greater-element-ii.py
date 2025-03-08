f42ca80f7cc17a3182b0aef8661cfd8fe1c205fb
Daven <19704779+MSoup@users.noreply.github.com>
2025-03-08 10:01:01 +0900

    Time: 27 ms (49.31%) | Memory: 19.5 MB (44.24%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -5,23 +5,36 @@ class Solution:
         # i = 4
         # len(nums) - i - 1 == j
 
-        found = False
+        s = []
+        for i in range(len(nums) * 2):
+            idx = i % len(nums) 
+            while s and nums[idx] > nums[s[-1]]:
+                top = s.pop()
+                res[top] = nums[idx]
+            
+            s.append(idx)
 
-        for j in range(len(nums)):
-            curr_num = nums[j]
-            found = False
-            for i in range(j, len(nums)):
-                next_num = nums[i]
-                if next_num > curr_num:
-                    res[j] = next_num
-                    found = True
-                    break
-            for i in range(0,j):
-                if found == True:
-                    break
-                next_num = nums[i]
-                if next_num > curr_num:
-                    res[j] = next_num
-                    break
-        
         return res
+
+
+        # found = False
+
+        # for j in range(len(nums)):
+        #     curr_num = nums[j]
+        #     found = False
+        #     for i in range(j, len(nums)):
+        #         next_num = nums[i]
+        #         if next_num > curr_num:
+        #             res[j] = next_num
+        #             found = True
+        #             break
+        #     for i in range(0,j):
+        #         if found == True:
+        #             break
+        #         next_num = nums[i]
+        #         if next_num > curr_num:
+        #             res[j] = next_num
+        #             break
+        
+        # return res
+
