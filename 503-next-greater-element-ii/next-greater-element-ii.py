cf23784c5ac2766544b004f1216a892c7dc80fd5
Daven <19704779+MSoup@users.noreply.github.com>
2025-03-08 09:41:49 +0900

    Time: 1185 ms (10.42%) | Memory: 19.2 MB (94.78%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        res = [-1] * len(nums)
+        # len(nums) = 5
+        # i = 4
+        # len(nums) - i - 1 == j
+
+        found = False
+
+        for j in range(len(nums)):
+            curr_num = nums[j]
+            found = False
+            for i in range(j, len(nums)):
+                next_num = nums[i]
+                if next_num > curr_num:
+                    res[j] = next_num
+                    found = True
+                    break
+            for i in range(0,j):
+                if found == True:
+                    break
+                next_num = nums[i]
+                if next_num > curr_num:
+                    res[j] = next_num
+                    break
+        
+        return res
